e0099ea6160fa3df45fb7fa05645b4fe5c14b048
Varun Bhangre <varunbhangre.11@gmail.com>
2021-08-30 18:30:35 +0530

    new commit

--- frontend/learnit/src/components/subjectComponent.js
+++ frontend/learnit/src/components/subjectComponent.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const subjectComponent = () => {
   return (
-    <div className="subject-container">
-      <div className="subject-img">
-        <img src="https://s3-alpha-sig.figma.com/img/ed5b/920c/37049a5a16169000f5349a0c5afd2ab6?Expires=1630886400&Signature=J9WYmNTrQpfG1IUl7zFr1VihIh1MEnxmA0KqxNh7Jm3qScmpiCHrgGzg95JVnu2eJeuvFeKNPLB5VkgGOcZbJJ3WfEHnc2fMeFdmR4gmn3zMuUQElWPhdwwL10CaXZ32uwSi~fNKhYoVj8F6jQf4FlHI7KDfbe7wJZ~lWF~nVkwkmde9kUQxcL9cp6wKzwfWG02nFsZsxipw2FSWn8610c5CotXcNAnx1Lw5PiL5eCMv7tmcF6yMdtFYYIC33LbYChOWhjXv4tiRx7cIexL57I925k14haJL8D6-u1LWr78WRVsUwdpCDkmCX4~-ouFVrJGrNbgCTng49O0vWxR8Mw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="pic" />
+      <div className="subject-container">
+        <div className="subject-img">
+          <img src="https://s3-alpha-sig.figma.com/img/ed5b/920c/37049a5a16169000f5349a0c5afd2ab6?Expires=1630886400&Signature=J9WYmNTrQpfG1IUl7zFr1VihIh1MEnxmA0KqxNh7Jm3qScmpiCHrgGzg95JVnu2eJeuvFeKNPLB5VkgGOcZbJJ3WfEHnc2fMeFdmR4gmn3zMuUQElWPhdwwL10CaXZ32uwSi~fNKhYoVj8F6jQf4FlHI7KDfbe7wJZ~lWF~nVkwkmde9kUQxcL9cp6wKzwfWG02nFsZsxipw2FSWn8610c5CotXcNAnx1Lw5PiL5eCMv7tmcF6yMdtFYYIC33LbYChOWhjXv4tiRx7cIexL57I925k14haJL8D6-u1LWr78WRVsUwdpCDkmCX4~-ouFVrJGrNbgCTng49O0vWxR8Mw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="pic" />
+        </div>
+        <div className="subject-info">
+          <b>Subject: Subject</b>
+          <p>Teacher: Name</p>
+        </div>
       </div>
-      <div className="subject-info">
-        <b>Subject: Subject</b>
-        <p>Teacher: Name</p>
-      </div>
-    </div>
   )
 }
 
---
+++ frontend/learnit/src/components/weeklyTests.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const weeklyTests = () => {
+  return (
+    <>
+      <h3>Test - Mathematics</h3>
+      <div className='test-info'>
+        <p>Chapters: 3,4,5</p>
+      </div>
+      <span>Date: 28-08-2021</span>
+    </>
+  )
+}
+
+export default weeklyTests
